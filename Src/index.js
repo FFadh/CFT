91de79e208e276df319fab0a9bb24b3f7042ebe7
FFadh <faisal.fadhillah@shipper.id>
2025-03-07 01:06:26 +0700

    Add files via upload

---
+++ Src/index.js
@@ -0,0 +1,30 @@
+var BASE_URL = "https://ffadh.github.io/trello-powerup/public"; // Sesuaikan dengan GitHub Pages Anda
+
+window.TrelloPowerUp.initialize({
+    "card-back-section": function (t, options) {
+        return {
+            title: "Custom Fields",
+            icon: BASE_URL + "/icon.png",
+            content: {
+                type: "iframe",
+                url: t.signUrl(BASE_URL + "/card.html"),
+                height: 200
+            }
+        };
+    },
+    "board-buttons": function (t, options) {
+        return [
+            {
+                icon: BASE_URL + "/icon.png",
+                text: "Settings",
+                callback: function (t) {
+                    return t.popup({
+                        title: "Custom Field Settings",
+                        url: BASE_URL + "/settings.html",
+                        height: 250
+                    });
+                }
+            }
+        ];
+    }
+});
---
+++ server.js
@@ -0,0 +1,15 @@
+{
+  "name": "Custom Field Power-Up",
+  "capabilities": [
+    "card-back-section",
+    "attachment-sections",
+    "board-buttons",
+    "card-buttons",
+    "settings"
+  ],
+  "connectors": {
+    "iframe": {
+      "url": "https://ffadh.github.io/trello-powerup/"
+    }
+  }
+}
